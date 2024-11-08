2259fbdbbfa37c2c7ae85e6530ccadd9bb8e20a9
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-11-08 18:54:45 +0000

    feat(addon): sync addon playlist and vehicles to repo

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 py combine.py --directory "src/addon" --destination "src/addon/script.lua" --allow_file_extension ".lua"
 py sync.py --directory "src/addon/script.lua" --destination "%appdata%/Stormworks/data/missions/Raft/"
+py sync.py --directory "%appdata%/Stormworks/data/missions/Raft" --destination "src/addon" --ignore "%appdata%/Stormworks/data/missions/Raft/script.lua"
 py sync.py --directory "src/mod" --destination "%appdata%/Stormworks/data/mods/Raft"
